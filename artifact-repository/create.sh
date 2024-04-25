e7115b77f4abd4d1586807cd4c2feb1bee2d2368
Anna Levenberg <alevenb@google.com>
2024-04-25 14:12:14 -0400

    impl(artifact-repository): add artifact repository info

---
+++ artifact-repository/create.sh
@@ -0,0 +1,11 @@
+LOCATION="us-central1"
+PROJECT_ID="climateiq-test"
+REPOSITORY="citycat-repo"
+
+# Create the repository
+gcloud artifacts repositories create ${REPOSITORY} \
+    --repository-format=docker \
+  --location=${LOCATION} \
+    --description="Store the City Cat docker image" \
+    --immutable-tags \
+    --async
